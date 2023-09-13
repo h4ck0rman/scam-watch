c7ef38689b571635cc0d7230106949f1377cbaef
h4ck0rman <h4ck0rman@gmail.com>
2023-09-13 08:08:39 -0400

    badly written server side code

--- app/src/routes/+page.svelte
+++ app/src/routes/+page.svelte
@@ -1,9 +1,10 @@
 <script>
-    export let data;
-    const { response } = data;
-    console.log('response from server:')
-    console.log(data)
+  export let data;
+  const { message } = data
+  console.log(message)
+  console.log('gotit')
 </script>
 
 <h1>Welcome to SvelteKit</h1>
 <p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+<p>{message}</p>
